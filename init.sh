4245576d690ab008b74533306a70e3b4aea6037d
atton <e115763@gmail.com>
2017-11-28 21:43:10 +0900

    Split script

--- init.sh
+++ init.sh
@@ -12,4 +12,3 @@ cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 sudo make install
-sudo /usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:5555 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 1 >& /dev/null
