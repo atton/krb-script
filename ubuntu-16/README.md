656797192b419df9e8efa628792c07de18b89331
atton <e115763@gmail.com>
2017-12-03 07:20:55 +0900

    init.sh for Ubuntu

---
+++ ubuntu-16/README.md
@@ -0,0 +1,1 @@
+Provisioning Script for KRB using RHEL7 or Ubuntu 16.04
---
+++ ubuntu-16/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd `dirname $0`
+
+sudo apt-get update
+sudo apt-get install -y git zsh powertop vim libcurl3 pciutils automake autoconf make gcc g++
+sudo chsh -s /bin/zsh $USER
+git clone https://github.com/atton/dot_files $HOME/dot_files
+cd $HOME/dot_files
+./install.sh
