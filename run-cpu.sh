009302012e689fdfa3bc0e8218b87cceca55e14f
atton <e115763@gmail.com>
2017-12-02 10:41:30 +0900

    Add comment

--- run-cpu.sh
+++ run-cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 
+# please tune thread number (-t option)
 sudo pkill minerd
 sudo /usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 1 >& /dev/null
-sudo powertop --auto-tune
