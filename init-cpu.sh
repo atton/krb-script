a42cdf89ed559967c8999ce972305f0bcff88a56
atton <e115763@gmail.com>
2017-12-02 10:31:57 +0900

    Split init script

---
+++ init-cpu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd `dirname $0`
+
+sudo yum install -y autoconf automake libcurl-devel git zsh gcc make powertop
+git clone https://github.com/OhGodAPet/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+sudo make install
--- init.sh
+++ init.sh
@@ -5,10 +5,4 @@ sudo yum install -y autoconf automake libcurl-devel git zsh gcc make powertop
 git clone https://github.com/atton/dot_files $HOME/dot_files
 cd $HOME/dot_files
 ./install.sh
-cd $HOME
-git clone https://github.com/OhGodAPet/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-CFLAGS="-march=native" ./configure
-sudo make install
 sudo powertop --auto-tune
