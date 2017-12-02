5617c056523734e5bc5acf67d1a3eb6471669e2d
atton <e115763@gmail.com>
2017-12-02 10:33:13 +0900

    Add chsh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 cd `dirname $0`
 
 sudo yum install -y autoconf automake libcurl-devel git zsh gcc make powertop
+sudo chsh -s /bin/zsh $USER
 git clone https://github.com/atton/dot_files $HOME/dot_files
 cd $HOME/dot_files
 ./install.sh
