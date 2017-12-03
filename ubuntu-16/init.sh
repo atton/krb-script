73f79f1057956a679948fad5384371a23a894274
atton <e115763@gmail.com>
2017-12-03 23:24:24 +0900

    Use unar

--- ubuntu-16/init.sh
+++ ubuntu-16/init.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 sudo apt-get update
-sudo apt-get install -y git zsh powertop vim libcurl3 pciutils automake autoconf make gcc g++
+sudo apt-get install -y git zsh powertop vim libcurl3 pciutils automake autoconf make gcc g++ unar
 sudo chsh -s /bin/zsh $USER
 git clone https://github.com/atton/dot_files $HOME/dot_files
 cd $HOME/dot_files
