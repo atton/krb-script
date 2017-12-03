1065fed42cbcba5bbdb88458cc90550de17993bf
atton <e115763@gmail.com>
2017-12-03 17:31:49 +0900

    Use sgminer

--- osx/init-amd.sh
+++ osx/init-amd.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 
-git clone https://github.com/xbbricker/cpuminer-multi-opencl
-cd cpuminer-multi-opencl
-./autogen.sh
-CFLAGS="-march=native" ./configure
+git clone https://github.com/sgminer-dev/sgminer
+cd sgminer
+git checkout -f 5.1.1
+git submodule init
+git submodule update
+autoreconf -i
+CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure
 make
