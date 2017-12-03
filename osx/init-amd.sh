31d1d7bad815f9ed4c5a69c2ac6f75a6f46e7bac
atton <e115763@gmail.com>
2017-12-03 19:48:18 +0900

    Update

--- osx/init-amd.sh
+++ osx/init-amd.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 cd `dirname $0`
 
-git clone https://github.com/sgminer-dev/sgminer
-cd sgminer
-git checkout -f 5.1.1
-git submodule init
-git submodule update
+brew install libtool curl pkg-config unar wget
+wget https://github.com/sgminer-dev/sgminer/archive/5.1.1.zip
+unar 5.1.1.zip
+cd sgminer-5.1.1
 autoreconf -i
-CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure
+CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure --without-curses 
 make
