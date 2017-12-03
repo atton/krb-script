9fa95790f9cc916e4dbb5fe795a7209906aaa723
atton <e115763@gmail.com>
2017-12-03 17:19:01 +0900

    Add run-amd.sh

---
+++ osx/run-amd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd `dirname $0`
+./cpuminer-multi-opencl/bin/minerd -B -q -a cryptonight -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x
