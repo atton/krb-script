4ccd89b65f7aa156700e1e67c6f2e66f38e848c2
atton <e115763@gmail.com>
2017-12-04 01:45:08 +0900

    Add init-amd.sh

--- ubuntu-16/init-amd.sh
+++ ubuntu-16/init-amd.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 cd `dirname $0`
 
-sudo apt-get install -y wget unar
+sudo apt-get -f install -y wget unar dpkg-dev autoconf automake make libcurl4-openssl-dev pkg-config libtool libncurses5-dev m4
+sudo apt-get autoremove
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get -f install
+
 wget --referer='https://www2.ati.com' https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-17.40-492261.tar.xz
 unar amdgpu-pro-17.40-492261.tar.xz
 cd amdgpu-pro-17.40-492261/
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
-ls *amd64*deb | xargs sudo dpkg -i 
+sudo dpkg -i -R  .
+sudo apt-get -f install
+sudo dpkg -i -R  .
+sudo apt-get -f install
+sudo dpkg -i -R  .
+sudo apt-get -f install
+sudo dpkg -i -R  .
+sudo apt-get -f install
+sudo dpkg -i -R  .
+sudo apt-get -f install
 
 
 git clone https://github.com/genesismining/sgminer-gm
