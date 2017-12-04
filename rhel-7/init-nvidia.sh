7ce37a722f3222f3db2bd17db789e967cf468de5
atton <e115763@gmail.com>
2017-12-05 00:20:59 +0900

    Use XMRig

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 yum install -y epel-release
-yum install --enablerepo=epel -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget git libtool-ltdl-devel autoconf-archive libtool opencl-headers unzip
+yum install --enablerepo=epel -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget git unzip libstdc++-static
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 yum install -y cuda-repo-rhel7-8.0.61-1.x86_64.rpm
@@ -10,18 +10,10 @@ yum clean all
 yum install -y cuda-8-0
 rm cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 
-wget http://registrationcenter-download.intel.com/akdlm/irc_nas/11396/SRB5.0_linux64.zip
-unzip SRB5.0_linux64.zip
-yum install -y intel-opencl-r5.0-63503.x86_64.rpm
-yum install -y intel-opencl-devel-r5.0-63503.x86_64.rpm
-
 modprobe nvidia
 
-git clone https://github.com/nicehash/sgminer-gm
-cd sgminer-gm
-git checkout -f 5.5.5-8
-git submodule init
-git submodule update
-autoreconf -i
-CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure --without-curses
-make install
+wget https://github.com/xmrig/xmrig-nvidia/archive/v2.4.2.zip
+unzip v2.4.2.zip
+cd xmrig-nvidia-2.4.2
+cmake3 . -DWITH_HTTPD=OFF
+make
