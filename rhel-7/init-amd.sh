00cb25d7f051747ac76da92348a0a599260aeffa
atton <e115763@gmail.com>
2017-12-04 17:16:26 +0900

    Update

--- rhel-7/init-amd.sh
+++ rhel-7/init-amd.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
 
-sudo yum install -y wget kernel-tools yum-utils
+sudo yum install -y wget kernel-tools yum-utils epel-release
+sudo yum --enablerepo=epel install -y dkms
 # wget --referer='https://www2.ati.com' https://www2.ati.com/drivers/linux/rhel7/amdgpu-pro-17.30-458935.tar.xz
 tar Jxfv amdgpu-pro-17.30-458935.tar.xz
 cd amdgpu-pro-17.30-458935
