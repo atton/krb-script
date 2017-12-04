4da58549de7404c2f5c798835978ffaccf3ccbcc
atton <e115763@gmail.com>
2017-12-04 20:31:39 +0900

    clone ccminer

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -3,14 +3,16 @@ cd `dirname $0`
 
 yum install -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget git
 
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/384.98/NVIDIA-Linux-x86_64-384.98.run
-# sh NVIDIA-Linux-x86_64-384.98.run # tui
-
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
-rpm -i cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
+wget http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-9.0.176-1.x86_64.rpm
+rpm -i cuda-repo-rhel7-9.0.176-1.x86_64.rpm
 yum clean all
-yum install cuda
+yum install -y cuda
+# wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
+# sh NVIDIA-Linux-x86_64-384.98.run # tui
+rm cuda-repo-rhel7-9.0.176-1.x86_64.rpm
+
 modprobe nvidia
 
-wget https://github.com/genesismining/sgminer-gm/releases/download/5.5.5/sgminer_ubuntu64 -O /usr/local/bin/sgminer
-chmod 755 /usr/local/bin/sgminer
+git clone https://github.com/tpruvot/ccminer
+cd ccminer
+git checkout -f 2.2.2-tpruvot
