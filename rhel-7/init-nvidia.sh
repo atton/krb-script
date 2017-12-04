a40ed15ae91029c586a4f7c0262e416e80ab80c5
atton <e115763@gmail.com>
2017-12-04 18:20:18 +0900

    Add NVIDIA driver

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 
-yum install -y wget
+yum install -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget
+
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/384.98/NVIDIA-Linux-x86_64-384.98.run
+# sh NVIDIA-Linux-x86_64-384.98.run # tui
+
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
 rpm -i cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
 yum clean all
