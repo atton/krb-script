e8959bf4c47eb6d2c3506155496e3f6fad83e648
atton <e115763@gmail.com>
2017-12-04 13:31:23 +0900

    Add modprobe

--- ubuntu-16/init.sh
+++ ubuntu-16/init.sh
@@ -4,6 +4,7 @@ cd `dirname $0`
 sudo apt update
 sudo apt-get upgrade -y
 sudo apt-get install -y git zsh powertop vim libcurl3 pciutils automake autoconf make gcc g++ unar
+sudo modprobe fglrx
 sudo chsh -s /bin/zsh $USER
 git clone https://github.com/atton/dot_files $HOME/dot_files
 cd $HOME/dot_files
