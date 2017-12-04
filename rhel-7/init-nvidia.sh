863e0a1973d753daacb52fa712faa980c506c1c2
atton <e115763@gmail.com>
2017-12-04 22:13:15 +0900

    Use CUDA 7.5

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -6,7 +6,7 @@ yum install -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build w
 wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 yum install -y cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 yum clean all
-yum install -y cuda-8-0
+yum install -y cuda-7-5
 rm cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 
 modprobe nvidia
