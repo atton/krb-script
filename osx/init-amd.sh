e57ea8418607dea046440698719f107c22321e35
atton <e115763@gmail.com>
2017-12-04 09:18:09 +0900

    Use compiled binary

--- osx/init-amd.sh
+++ osx/init-amd.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 
-brew install libtool curl pkg-config unar wget
-wget https://github.com/sgminer-dev/sgminer/archive/5.1.1.zip
-unar 5.1.1.zip
-rm 5.1.1.zip
-cd sgminer-5.1.1
-git submodule init
-git submodule update
-autoreconf -i
-CFLAGS="-O2 -Wall -march=native -std=gnu99" ./configure --without-curses
-make
+brew install unar
+wget 'http://www.spaceman.ca/sgminer/download.php?Ver=latest&Type=Miner' -O sgminer.zip
+unar sgminer.zip
