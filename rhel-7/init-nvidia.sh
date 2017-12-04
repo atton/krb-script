5b960a311e5385821c39c0e61c1b4e45858144a7
atton <e115763@gmail.com>
2017-12-04 18:09:27 +0900

    Remove sudo

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd `dirname $0`
 
-sudo yum install -y wget
+yum install -y wget
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
-sudo rpm -i cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
-sudo yum clean all
-sudo yum install cuda
-sudo modprobe nvidia
+rpm -i cuda-repo-rhel7-9-0-local-9.0.176-1.x86_64-rpm
+yum clean all
+yum install cuda
+modprobe nvidia
 
 git clone https://github.com/tpruvot/ccminer/ $HOME/ccminer
 cd $HOME/ccminer
 ./build.sh
-sudo make install
+make install
