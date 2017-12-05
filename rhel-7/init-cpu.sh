17cf999674fa6e5fc46a4e19786aa2560be30901
atton <e115763@gmail.com>
2017-12-04 21:02:29 -0500

    Add --disable-aes-ni

--- rhel-7/init-cpu.sh
+++ rhel-7/init-cpu.sh
@@ -5,5 +5,6 @@ sudo yum install -y autoconf automake libcurl-devel git zsh gcc make powertop
 git clone https://github.com/OhGodAPet/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
+# CFLAGS="-march=native" ./configure --disable-aes-ni
 CFLAGS="-march=native" ./configure
 sudo make install
