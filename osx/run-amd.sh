e99d0feed38751326878e5eade570b32cfde5c10
atton <e115763@gmail.com>
2017-12-05 13:35:26 +0900

    Fix path

--- osx/run-amd.sh
+++ osx/run-amd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-./sgminer-4.1.0-mac-slim/sgminer -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+/opt/krb-script/osx/sgminer-4.1.0-mac-slim/sgminer -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
