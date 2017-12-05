f6d93dcc99a386e70efdc57552e2e10bf124d450
atton <e115763@gmail.com>
2017-12-04 19:51:18 -0500

    Use sgminer

--- rhel-7/init-amd.sh
+++ rhel-7/init-amd.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cd `dirname $0`
 
-sudo yum install -y wget kernel-tools yum-utils epel-release kernel-devel unzip rpm-build
-sudo yum --enablerepo=epel install -y dkms
-wget --referer='https://www2.ati.com' https://www2.ati.com/drivers/linux/amd-catalyst-15.9-linux-installer-15.201.1151-x86.x86_64.zip
-unzip amd-catalyst-15.9-linux-installer-15.201.1151-x86.x86_64.zip
-sh AMD-Catalyst-15.9-Linux-installer-15.201.1151-x86.x86_64.run --extract hoge
+yum install -y wget unzip xorg-x11-drivers openssl-devel cmake3 gcc-c++
+wget --referer='https://www2.ati.com' https://www2.ati.com/drivers/linux-amd-14.41rc1-opencl2-sep19.zip
+unzip linux-amd-14.41rc1-opencl2-sep19.zip
+sh fglrx-14.41
+sh amd-driver-installer-14.41-x86.x86_64.run --extract hoge
 cd hoge
-sudo sh ati-installer.sh -- --buildpkg RedHat/RHEL7_64a --NoXServer
-cd ..
-sudo rpm -i fglrx64_p_i_c-15.201.1151-1.x86_64.rpm
+./ati-installer.sh  -- --buildpkg RedHat/RHEL7_64a --NoXServer
+yum install -y ../fglrx64_p_i_c-14.41-1.x86_64.rpm
+modprobe fglrx
+
+wget https://github.com/genesismining/sgminer-gm/releases/download/5.5.5/sgminer_ubuntu64
+mv ./sgminer_ubuntu64 /usr/local/bin/sgminer
+chmod 755 /usr/local/bin/sgminer
