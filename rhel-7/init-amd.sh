10feb81ad517285e106527ca7b9e03ed29eafe33
atton <atton@macallan.ns.ie.u-ryukyu.ac.jp>
2017-12-05 12:27:00 +0900

    Fix

--- rhel-7/init-amd.sh
+++ rhel-7/init-amd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 
-yum install -y wget unzip xorg-x11-drivers openssl-devel cmake3 gcc-c++
+yum install --enablerepo=epel -y wget unzip xorg-x11-drivers openssl-devel cmake3 gcc-c++
 wget --referer='https://www2.ati.com' https://www2.ati.com/drivers/linux-amd-14.41rc1-opencl2-sep19.zip
 unzip linux-amd-14.41rc1-opencl2-sep19.zip
 sh fglrx-14.41
