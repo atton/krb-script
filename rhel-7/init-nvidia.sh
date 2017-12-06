3ef534a486aa317a9817fa97700a79007ca1b06c
atton <e115763@gmail.com>
2017-12-06 23:00:09 +0900

    Fix rhel-7/init-nvidia

--- rhel-7/init-nvidia.sh
+++ rhel-7/init-nvidia.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 yum install -y epel-release
-yum install --enablerepo=epel -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget git unzip libstdc++-static
+yum install --enablerepo=epel -y wget kernel-tools yum-utils epel-release kernel-devel rpm-build wget git unzip libstdc++-static cmake3
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-8.0.61-1.x86_64.rpm
 yum install -y cuda-repo-rhel7-8.0.61-1.x86_64.rpm
@@ -17,3 +17,4 @@ unzip v2.4.2.zip
 cd xmrig-nvidia-2.4.2
 cmake3 . -DWITH_HTTPD=OFF
 make
+cd ../v2.4.2.zip
