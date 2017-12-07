b0ea77c80913b53c5ad30bb3d8b1388245724de9
atton <e115763@gmail.com>
2017-12-07 00:43:33 -0500

    Fix

--- rhel-7/init.sh
+++ rhel-7/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 
-yum install -y autoconf automake libcurl-devel git zsh gcc make powertop pciutils epel-release
+sudo yum install -y autoconf automake libcurl-devel git zsh gcc make powertop pciutils epel-release
 chsh -s /bin/zsh $USER
 git clone https://github.com/atton/dot_files $HOME/dot_files
 cd $HOME/dot_files
