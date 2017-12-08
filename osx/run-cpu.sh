20d7cf7009d122ddd29a2d779434a0a6607d7585
atton <e115763@gmail.com>
2017-12-08 10:13:31 +0900

    Fix endpoint for osx

--- osx/run-cpu.sh
+++ osx/run-cpu.sh
@@ -2,5 +2,5 @@
 cd `dirname $0`
 
 # please tune thread number (-t option)
-/usr/local/bin/xmrig -t 2 -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+/usr/local/bin/xmrig -t 2 -o stratum+tcp://us2.mininggood.com:3353 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
--- osx/run-nvidia.sh
+++ osx/run-nvidia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 
-./ccminer-cryptonight-mac/bin/ccminer -a cryptonight -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+./ccminer-cryptonight-mac/bin/ccminer -a cryptonight -o stratum+tcp://us2.mininggood.com:3353 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
