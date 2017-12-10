8335ef2f2f2b9e7db5f37940c5f1e6fc85b6c159
atton <e115763@gmail.com>
2017-12-10 23:16:17 +0900

    Increase difficulty using GPU with osx

--- osx/run-amd.sh
+++ osx/run-amd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 pkill sgminer
-/opt/krb-script/osx/sgminer-4.1.0-mac-slim/sgminer -o stratum+tcp://pool.krbpool.com:33337 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+/opt/krb-script/osx/sgminer-4.1.0-mac-slim/sgminer -o stratum+tcp://pool.krbpool.com:33338 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
--- osx/run-nvidia.sh
+++ osx/run-nvidia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 
-./ccminer-cryptonight-mac/bin/ccminer -a cryptonight -o stratum+tcp://pool.krbpool.com:33337 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+./ccminer-cryptonight-mac/bin/ccminer -a cryptonight -o stratum+tcp://pool.krbpool.com:33338 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
