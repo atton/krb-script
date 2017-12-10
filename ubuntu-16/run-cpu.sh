e6137f31fbf3575df5d194ece72a4821fdeb00b7
atton <e115763@gmail.com>
2017-12-10 02:06:13 +0000

    Fix cpu

---
+++ ubuntu-16/run-cpu.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+cd `dirname $0`
+
+pkill minerd
+/usr/local/bin/minerd -t 4 -B -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x &> /dev/null
