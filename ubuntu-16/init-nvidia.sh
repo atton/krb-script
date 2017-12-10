13ec013117bbc957adc8c7c17f42db93202ad274
atton <e115763@gmail.com>
2017-12-10 10:32:53 +0800

    Fix

--- ubuntu-16/init-nvidia.sh
+++ ubuntu-16/init-nvidia.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 
 apt update -y
 apt upgrade -y
-apt-get install -y wget
+apt-get install -y wget libcurl4-openssl-dev libuv1-dev
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
