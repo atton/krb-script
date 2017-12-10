a6725dd55e6eb8930ecabb8712bda49480ca390e
atton <e115763@gmail.com>
2017-12-10 20:42:54 +0900

    Use krbpool.com

--- ubuntu-16/run-amd.sh
+++ ubuntu-16/run-amd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-/usr/local/bin/sgminer  -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
+/usr/local/bin/sgminer -o stratum+tcp://pool.krbpool.com:33337 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x >& /dev/null &
 disown $1
--- ubuntu-16/run-cpu.sh
+++ ubuntu-16/run-cpu.sh
@@ -2,4 +2,4 @@
 cd `dirname $0`
 
 pkill minerd
-/usr/local/bin/minerd -t 4 -B -o stratum+tcp://krb.crypto-coins.club:6666 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x &> /dev/null
+/usr/local/bin/minerd -t 4 -B -o stratum+tcp://pool.krbpool.com:33337 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x &> /dev/null
