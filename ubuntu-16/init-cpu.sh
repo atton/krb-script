669792436c27d77419649400710c30f7b9f0bc18
atton <e115763@gmail.com>
2017-12-21 09:44:38 +0900

    Use apt-get update

--- ubuntu-16/init-cpu.sh
+++ ubuntu-16/init-cpu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 
+sudo apt-get update
 sudo apt-get install -y autoconf automake libcurl4-openssl-dev git zsh gcc make powertop
 git clone https://github.com/OhGodAPet/cpuminer-multi
 cd cpuminer-multi
