a44c607e30035628c58080f32993af0bd6b3e794
atton <e115763@gmail.com>
2017-12-21 10:19:40 +0900

    Add 16 core centos script

---
+++ gcp/centos7-16.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum install -y autoconf automake libcurl-devel git zsh gcc make
+git clone https://github.com/OhGodAPet/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+sudo make install
+sudo /usr/local/bin/minerd -B -q -a cryptonight -o stratum+tcp://pool.krbpool.com:33338 -u KgGc79QyYag4LugEc2Gnxobt7Mfe9CSEh4NqGzdysooL3Czy6mKtuqLiCehzgoBDESTaYDN3U9XtgLxVJLmPCi397ppx6cL -p x -t 16 >& /dev/null
+disown $1
